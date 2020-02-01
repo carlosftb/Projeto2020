3ce954a02467f0418ecd539a045fc80726d8a5f7
Carlos David <carlos.david.ftb@hotmail.com>
2020-02-01 19:56:57 -0300

    Scafold

---
+++ Projeto2020/Data/Projeto2020Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto2020.Models;
+
+namespace Projeto2020.Data
+{
+    public class Projeto2020Context : DbContext
+    {
+        public Projeto2020Context (DbContextOptions<Projeto2020Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto2020.Models.Categoria> Categoria { get; set; }
+    }
+}
--- Projeto2020/Startup.cs
+++ Projeto2020/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Projeto2020.Data;
 
 namespace Projeto2020
 {
@@ -33,6 +35,9 @@ namespace Projeto2020
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Projeto2020Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Projeto2020Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
